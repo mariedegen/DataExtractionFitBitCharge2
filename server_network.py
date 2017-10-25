5f891049ae4711a851366f2b6552d905337fabaf
mariedegen <marie.degen@live.fr>
2017-10-25 21:58:47 +0200

    network changes

--- server_network.py
+++ server_network.py
@@ -1,34 +1,49 @@
-from bluetooth import *
+#!/usr/bin/python
 
+import bluetooth
+import threading
+import time
+import random
+import sys
 
-def serverNetwork(port, bluetooth_MAC_address):
-##    //Non-privileged port
-##    port = 8888 
-##    bluetooth_MAC_address = "B8-81-98-D4-F2-78"
+tabData = []
+address="BC:76:5E:96:EE:6C"
+uuid="00001101-0000-1000-8000-00805F9B34FB" 
+#service_matches = bluetooth.find_service(uuid = uuid, address = address)
 
-    tabData = []
+while True:
+    print ("Searching ...")
+    try:
+            service_matches
+    except NameError:
+      service_matches = bluetooth.find_service(uuid = uuid)
+    else:
+      if not service_matches:
+            print ("without address")
+            service_matches = bluetooth.find_service(uuid = uuid)
+      else:
+            print ("with address")
+            service_matches_with_addr = bluetooth.find_service(uuid = uuid, address = host)
+            if service_matches_with_addr:
+              service_matches = service_matches_with_addr
+            else:
+              continue
 
-    server_socket = BluetoothSocket(RFCOMM)
-    server_socket.bind((bluetooth_MAC_address, port))
-    server_socket.listen(1)
+    if service_matches:
+      first_match = service_matches[0]
+      port = first_match["port"]
+      name = first_match["name"]
+      host = first_match["host"]
+      print ("Connecting to \"%s\" on %s") % (name, host)
+      sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+      sock.connect((host, port))
+      
+      while True: 
+          data = sock.recv(1024)
+          if data:
+            print("Received [%s]" %data)
+            tabData.append(data)
+          elif not data :
+            break
+sock.close()
 
-    while True:
-        client_socket, address = server_socket.accept()
-        try:
-            while True:
-                data = client_socket.recv(1024)
-                if data:
-                    print("received [%s]" %data)
-                    tabData.append(data)
-                    client_socket.send(data)
-                elif not data :
-                    break
-                            
-        except:
-            print("Closing socket")
-            client_socket.close()
-            server_socket.close()
-
-    return tabData
-
-		
